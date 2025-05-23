135d30c256c7b9aab648803bf1d636f2f516500d
DeeptiMishra1512 <mishra.deeptimishra@gmail.com>
2025-05-23 08:51:54 -0400

    First Python code committed

---
+++ Classes/addnumbers.py
@@ -0,0 +1,8 @@
+#This is my first Python practice code -
+# This code uses a class and a method within the class
+# The method adds numbers in an empty list datastructures
+
+
+class ListDataStructure:
+   listAddNumbers = []
+
--- DockerImageCreation/main.py
+++ DockerImageCreation/main.py
@@ -1,2 +1,3 @@
 print("Hello")
 print("This is my first Python Docker Image!!")
+print("Continuing to add some classes and DataStructure practice code")
